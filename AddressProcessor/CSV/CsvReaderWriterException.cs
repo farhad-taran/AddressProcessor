a48a6bdbf3f425089d1dff2757b4a9f59438b591
taranf <farhad.taran@umg.com>
2017-09-10 19:01:20 +0100

    Code Complete

---
+++ AddressProcessor/CSV/CsvReaderWriterException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressProcessing.CSV
+{
+    public class CsvReaderWriterException : ApplicationException
+    {
+        public CsvReaderWriterException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
